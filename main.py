647238008a7067d7a3ed6def1bea037a41b0c000
c881 <jacov.g@gmail.com>
2021-09-01 11:00:29 +0300

    Update main.py
    cleaning

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import quiz_brain
 questions_bank = []
 
 for quest in question_data:
-    # new_q = Question(question_data[idx]["text"], question_data[idx]["answer"])
     questions_bank.append(Question(quest["text"], quest["answer"]))
 
-for _ in questions_bank:
-    print(_)
+# for question in questions_bank:
+#     print(question)
 
 
