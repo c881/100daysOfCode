a6fd64bafb64f47645d2a9fdba5ef34e81de33c1
koby <jacov.g@gmail.com>
2021-09-22 15:07:36 +0300

    Day24 - MailMerged 2nd Option

--- Day24-Files/MailMerge/main.py
+++ Day24-Files/MailMerge/main.py
@@ -1,4 +1,4 @@
-#TODO: Create a letter using starting_letter.txt 
+#TODO: Create a letter using starting_letter.txt
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
@@ -11,9 +11,14 @@ with open("Input/Names/invited_names.txt", "r") as inv_names:
     names = [name[:-1] for name in inv_names.readlines()]
 
 with open("Input/Letters/starting_letter.txt", "r") as letter:
-    lines = [line for line in letter.readlines()]
+    # lines = [line for line in letter.readlines()]
+    letter_cont = letter.read()
 
 for name in names:
-    new_letter = [line.replace("[name]", name) for line in lines]
-    with open(f"Output/letter_for_{name}", "w") as out_letter:
-        out_letter.writelines(new_letter)
+    new_letter = letter_cont.replace("[name]", name)
+    with open(f"Output/ReadyToSend/letter_for_{name}", "w") as out_letter:
+        out_letter.write(new_letter)
+# for name in names:
+#     new_letter = [line.replace("[name]", name) for line in lines]
+#     with open(f"Output/ReadyToSend/letter_for_{name}", "w") as out_letter:
+#         out_letter.writelines(new_letter)
