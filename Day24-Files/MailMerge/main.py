514733e5b535426a1c0e0a5d234e49e9cd110f3f
c881 <jacov.g@gmail.com>
2021-09-23 05:12:19 +0300

    Day25 - prep

--- Day24-Files/MailMerge/main.py
+++ Day24-Files/MailMerge/main.py
@@ -1,22 +1,22 @@
-#TODO: Create a letter using starting_letter.txt
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-#Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-#Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-with open("Input/Names/invited_names.txt", "r") as inv_names:
+with open("./Input/Names/invited_names.txt", "r") as inv_names:
     names = [name.strip() for name in inv_names.readlines()]
 
-with open("Input/Letters/starting_letter.txt", "r") as letter:
+with open("./Input/Letters/starting_letter.txt", "r") as letter:
     # lines = [line for line in letter.readlines()]
     letter_cont = letter.read()
 
 for name in names:
     new_letter = letter_cont.replace("[name]", name)
-    with open(f"Output/ReadyToSend/letter_for_{name}.txt", "w") as out_letter:
+    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as out_letter:
         out_letter.write(new_letter)
 # for name in names:
 #     new_letter = [line.replace("[name]", name) for line in lines]
