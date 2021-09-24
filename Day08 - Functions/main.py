30c46dd28536ec9957b7eb684a96af0d0c652157
c881 <jacov.g@gmail.com>
2021-09-24 22:59:04 +0300

    Adding startingDays.

---
+++ Day08 - Functions/main.py
@@ -0,0 +1,21 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet(name):
+    print("Greeting")
+    print(f"Hello {name}")
+    print("See you later")
+
+
+greet("koby")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"how is it like in {location}?")
+
+def greet_with_keyword(name, location):
+    print(f"Hello {name}")
+    print(f"how is it like in {location}?")
+
---
+++ Day09 - BlindAuction/main.py
@@ -0,0 +1,27 @@
+frm os import system, name
+from art import logo
+# HINT: You can call clear() to clear the output in the console.
+def clear():
+    if name == "nt":
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+auction = {}
+more_bidders = "yes"
+print(logo)
+print("Welcome to the secret auction program")
+while more_bidders == "yes":
+    bidder_name = input("What is your name? ")
+    bidding = round(float(input("what is your bid? $")),2)
+    auction[bidder_name] = bidding
+    more_bidders = input("Are there any other bidders? type 'yes' or 'no'\n").lower()
+    clear()
+top_bidder = ""
+top_bidding = -1
+for bidder in auction:
+    if auction[bidder] > top_bidding:
+        top_bidding = auction[bidder]
+        top_bidder = bidder
+print(f"The winner is {top_bidder} with a bid of ${top_bidding}")
+
