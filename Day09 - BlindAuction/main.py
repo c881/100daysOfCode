8bd3f7332d586f9001e80a499116931d53ab39a3
c881 <jacov.g@gmail.com>
2021-09-24 23:06:40 +0300

    Adding startingDays.

--- Day09 - BlindAuction/main.py
+++ Day09 - BlindAuction/main.py
@@ -1,11 +1,15 @@
-frm os import system, name
+from os import system, name
 from art import logo
+
 # HINT: You can call clear() to clear the output in the console.
+
+
 def clear():
     if name == "nt":
         _ = system('cls')
     else:
         _ = system('clear')
+
 
 auction = {}
 more_bidders = "yes"
@@ -13,7 +17,7 @@ print(logo)
 print("Welcome to the secret auction program")
 while more_bidders == "yes":
     bidder_name = input("What is your name? ")
-    bidding = round(float(input("what is your bid? $")),2)
+    bidding = round(float(input("what is your bid? $")), 2)
     auction[bidder_name] = bidding
     more_bidders = input("Are there any other bidders? type 'yes' or 'no'\n").lower()
     clear()
@@ -24,4 +28,3 @@ for bidder in auction:
         top_bidding = auction[bidder]
         top_bidder = bidder
 print(f"The winner is {top_bidder} with a bid of ${top_bidding}")
-
