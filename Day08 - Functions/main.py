6f48826ac6671709ecd4d5bb8d9bc91ab83544e6
koby <jacov.g@gmail.com>
2021-09-25 06:28:12 +0300

    Filling gaps.

--- Day08 - Functions/main.py
+++ Day08 - Functions/main.py
@@ -1,21 +1,61 @@
-# Review:
-# Create a function called greet().
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
+# # Review:
+# # Create a function called greet().
+# # Write 3 print statements inside the function.
+# # Call the greet() function and run your code.
+#
+# def greet(name):
+#     print("Greeting")
+#     print(f"Hello {name}")
+#     print("See you later")
+#
+#
+# greet("koby")
+#
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"how is it like in {location}?")
+#
+# def greet_with_keyword(name, location):
+#     print(f"Hello {name}")
+#     print(f"how is it like in {location}?")
+#
 
-def greet(name):
-    print("Greeting")
-    print(f"Hello {name}")
-    print("See you later")
+import art
+
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def sub(n1, n2):
+    return n1 - n2
+
+# Multiplay
+def mult(n1, n2):
+    return n1 * n2
+
+# Divide
+def div(n1, n2):
+    return n1 / n2
+
+# power
+def pow(n1, n2):
+    return n1 ** n2
 
 
-greet("koby")
+operations = {
+    '+': add,
+    '-': sub,
+    '*': mult,
+    '/': div,
+    '^': pow
+}
 
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"how is it like in {location}?")
-
-def greet_with_keyword(name, location):
-    print(f"Hello {name}")
-    print(f"how is it like in {location}?")
+num1 = int(input("what is your first number: "))
+num2 = int(input("what is your next number: "))
+for symbol in operations:
+    print(symbol)
+symbol = input("what is the operation you want to use: ")
+if symbol in operations:
+    print(f"{num1} {symbol} {num2} = {operations[symbol](num1, num2)}")
 
