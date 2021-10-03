8908c81c83f75c3efa4fddf5e1cc163f7404c3fb
koby <jacov.g@gmail.com>
2021-10-03 22:22:55 +0300

    Day27 - TkinterBegin

--- Day27 - Tkinter/main.py
+++ Day27 - Tkinter/main.py
@@ -8,4 +8,18 @@ window.minsize(width=500, height=300)
 my_label = tk.Label(text="hello again")
 my_label.pack()
 
+my_label.config(text='New Label')
+
+def button_clicked():
+    my_label["text"] = user_input.get()
+
+# Button
+my_button = tk.Button(text="Click me!", command=button_clicked)
+my_button.pack()
+
+# Entry
+user_input = tk.Entry(textvariable="hello there!")
+user_input.pack()
+
+
 window.mainloop()
