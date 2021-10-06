05fd96533d59dcd4b80a4f82e16cfb7eb249580f
c881 <jacov.g@gmail.com>
2021-10-06 05:11:05 +0300

    Day28 - Starting

--- Day28_Pomodoro_Tkinter/main.py
+++ Day28_Pomodoro_Tkinter/main.py
@@ -19,9 +19,15 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-canvas = Canvas(width=200, height=224)
+window.config(padx=60, pady=50, bg=YELLOW)
+
+# draw an image and text over it.
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112,image=tomato_img) # 100 & 112 are - x & y of the center of the image/ canvas
+                                               # 101, because of padding
+canvas.create_text(100, 124, text="00:00", font=(FONT_NAME, 35, "bold"))
+
 canvas.grid(column=0, row=0)
 
 window.mainloop()
