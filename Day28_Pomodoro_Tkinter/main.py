6553592778b08eb0e6de32893adf720590030972
koby <jacov.g@gmail.com>
2021-10-06 22:37:23 +0300

    Day28 - UI Done.

--- Day28_Pomodoro_Tkinter/main.py
+++ Day28_Pomodoro_Tkinter/main.py
@@ -10,7 +10,11 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_count():
+    pass
 
+def start_count():
+    pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -27,7 +31,17 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112,image=tomato_img) # 100 & 112 are - x & y of the center of the image/ canvas
                                                # 101, because of padding
 canvas.create_text(100, 124, text="00:00", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
-canvas.grid(column=0, row=0)
+timer_label = Label(text="Timer",fg="dark green",font=(FONT_NAME,30, "bold"), bg=YELLOW)
+timer_label.grid(column=1, row=0)
+start_button = Button(text="Start", command=start_count)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", command=reset_count)
+reset_button.grid(column=2, row=2)
+
+checkmarks_label = Label(text="✔", fg=GREEN)
+checkmarks_label.grid(column=1, row=3)
+
 
 window.mainloop()
