44147db8b84b7dfc9ecc906dc01c8b4f91385bee
c881 <jacov.g@gmail.com>
2021-10-16 06:49:51 +0300

    Day29 - Add find_password. day30 challenge.

--- Day29 - Password_Manager/main.py
+++ Day29 - Password_Manager/main.py
@@ -63,6 +63,10 @@ def save():
             #Clear screen
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+# ---------------------------- find PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    email = email_entry.get()
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -75,27 +79,29 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 #Labels
-website_label = Label(text="Website:")
+website_label = Label(text="Website:", width=20)
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
+email_label = Label(text="Email/Username:", width=20)
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
+password_label = Label(text="Password:", width=20)
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=25)
+website_entry.grid(row=1, column=1) #, columnspan=2)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=47)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "jacov.g@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
+search_button = Button(text="Search", width=20 , command=find_password)
+search_button.grid(row=1,column=2)
+generate_password_button = Button(text="Generate Password", width=20, command=generate_password)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button = Button(text="Add", width=25, command=save)
+add_button.grid(row=4, column=1) #, columnspan=2)
 
 window.mainloop()
