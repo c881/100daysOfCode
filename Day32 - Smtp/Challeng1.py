484e5bfd50bbbcce4b3070663497f68c0a37d296
c881 <jacov.g@gmail.com>
2021-10-19 22:36:12 +0300

    Day32 - Challenge1

--- Day32 - Smtp/Challeng1.py
+++ Day32 - Smtp/Challeng1.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import smtplib, ssl
-WEEKDAYS = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
+WEEKDAYS = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday',
+            5: 'Saturday', 6: 'Sunday'}
 
 # print (WEEKDAYS[dt.datetime.weekday(dt.datetime.now())])
 quates = {}
@@ -17,10 +18,10 @@ context = ssl.create_default_context()
 
 # Try to log in to server and send email
 # Like with files, using with to auto close the connection
-with smtplib.SMTP(smtp_server,port) as server:
+with smtplib.SMTP(smtp_server, port) as server:
     try:
     # server = smtplib.SMTP(smtp_server,port)
-        server.starttls(context=context) # Secure the connection
+        server.starttls(context=context)  # Secure the connection
         server.login(sender_email, password)
         server.sendmail(from_addr=sender_email,
                         to_addrs="jacov.g@gmail.com",
