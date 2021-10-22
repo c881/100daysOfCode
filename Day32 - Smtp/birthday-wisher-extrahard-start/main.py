8992afb0c7b1e11b68ce1d68b4ecc828a456a405
c881 <jacov.g@gmail.com>
2021-10-22 05:17:54 +0300

    Day32 - Challenge2 - Send random birthday letter. Succedded

--- Day32 - Smtp/birthday-wisher-extrahard-start/main.py
+++ Day32 - Smtp/birthday-wisher-extrahard-start/main.py
@@ -1,13 +1,50 @@
 ##################### Extra Hard Starting Project ######################
+import smtplib, ssl
+import datetime as dt
+import random as rd
+import pandas as pd
+
+def send_mail(message1):
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
+    sender_email = "kobygs78@gmail.com"
+    password = input("Type your password and press enter: ")
+    message = f"Subject: Happy Birthday \n\n {message1}"
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    # Try to log in to server and send email
+    # Like with files, using with to auto close the connection
+    with smtplib.SMTP(smtp_server, port) as server:
+        try:
+            # server = smtplib.SMTP(smtp_server,port)
+            server.starttls(context=context)  # Secure the connection
+            server.login(sender_email, password)
+            server.sendmail(from_addr=sender_email,
+                            to_addrs="jacov.g@gmail.com",
+                            msg=message)
+        except Exception as e:
+            # Print any error messages to stdout
+            print(e)
 
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME]
-# with the person's actual name from birthdays.csv
+today = dt.datetime.now()
 
-# 4. Send the letter generated in step 3 to that person's email address.
+birthdate_data = pd.read_csv("birthdays.csv")
+birth_dict = {row[1][0]: {"email": row[1][1],
+                         "month": row[1][3],
+                         "day":row[1][4]} for row in birthdate_data.iterrows()}
+for item in birth_dict.items():
+    # print(item[1]['email'])
+    if today.month == item[1]["month"] and today.day == item[1]["day"]:
+        with open(f"letter_templates/letter_{rd.randint(1, 4)}.txt") as letter:
+            letter_cont = letter.read()
+        letter_cont = letter_cont.replace("[NAME]", item[0])
+        send_mail(letter_cont)
+
 
 
 
