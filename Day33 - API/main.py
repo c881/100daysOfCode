1902593ef41d88beb699b0f77465e2fd445d6ce6
c881 <jacov.g@gmail.com>
2022-02-18 18:18:22 +0200

    Day33 - Using Api. Kanya west quote challenge.

--- Day33 - API/main.py
+++ Day33 - API/main.py
@@ -1,8 +1,14 @@
 """Trying to call the International Space Station(ISS) API for the current ISS Location.
 	Day33 of the course '100 Days of code'
 	"""
+import time
 
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+# print(response.content)
+for _ in range(5):
+	response = requests.get(url="http://api.kanye.rest")
+	text_quote = response.json()
+	print(text_quote["quote"])
+	time.sleep(3)
