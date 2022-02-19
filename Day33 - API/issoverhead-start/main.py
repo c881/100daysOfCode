4c21e6b63f3ddef6a3bba8ea121d6652d8a75fc5
c881 <jacov.g@gmail.com>
2022-02-19 06:55:34 +0200

    Day33 - Using Api. ISS Challenge

--- Day33 - API/issoverhead-start/main.py
+++ Day33 - API/issoverhead-start/main.py
@@ -1,8 +1,11 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+def is_iss_close():
+    return (abs(iss_latitude - MY_LAT) < 5) and (abs(iss_longitude - MY_LONG))
+    
+MY_LAT = 31.928855
+MY_LONG = 34.798594
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -12,7 +15,6 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 #Your position is within +5 or -5 degrees of the ISS position.
-
 
 parameters = {
     "lat": MY_LAT,
@@ -28,6 +30,8 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
+if (is_iss_close() and sunrise < time_now.hour > sunset):
+    pass
 #If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
