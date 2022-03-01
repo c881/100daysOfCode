082b69c651af355f94f4cbb016835cc41348b633
c881 <jacov.g@gmail.com>
2022-03-01 05:36:11 +0200

    Day34 - Html escaping. Quiz with API & GUI. Fixed bug

--- Day34-Trivya-Gui/quizzler-app-start/ui.py
+++ Day34-Trivya-Gui/quizzler-app-start/ui.py
@@ -9,35 +9,48 @@ class QuizInterface:
         self.quiz = quiz_brain
         self.window = tk.Tk()
         self.window.title("Quizzler")
-        self.window.config(padx=20, pady=20,bg=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
         self.score_label = tk.Label(text="Score:0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
-        self.canvas = tk.Canvas(width=300, height=250,bg="white")
-        self.qustion_text = self.canvas.create_text(150, 120, text="Question Goes HERE", width=280,
-                                               font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.canvas = tk.Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 120, text="Question Goes HERE", width=280,
+                                                     font=("Arial", 20, "italic"), fill=THEME_COLOR)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = tk.PhotoImage(file="images/true.png")
-
-        self.true_button = tk.Button(image=true_image, highlightthickness=0)  # , command=true)
-        self.true_button.grid(row=2, column=0)
         false_image = tk.PhotoImage(file="images/false.png")
-        self.false_button = tk.Button(image=false_image, highlightthickness=0)  # , command=false)
+        self.true_button = tk.Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+        self.false_button = tk.Button(image=false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
-
         self.get_next_question()
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.qustion_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions:
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            if q_text != "IndexError":
+                self.canvas.itemconfig(self.question_text, text=q_text)
 
+        if q_text == "IndexError":
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
-
-def main():
-    qi = QuizInterface()
-
-if __name__ == "main":
-    main()
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+        
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+    
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+    
